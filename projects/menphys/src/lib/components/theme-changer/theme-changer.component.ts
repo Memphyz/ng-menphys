b86ca2b2d4397532f3703f5aef3f08a9b2f54f87
Lucas Ribeiro <lucas2001.789@gmail.com>
2023-05-25 22:35:11 -0300

    melhoria no theme changer

--- projects/menphys/src/lib/components/theme-changer/theme-changer.component.ts
+++ projects/menphys/src/lib/components/theme-changer/theme-changer.component.ts
@@ -1,4 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MenphysModule } from '@menphys/menphys.module';
+import { Icon } from '@menphys/models/icon/icon';
+import * as dark from '@menphys/shared/themes/dark.json'
+import * as light from '@menphys/shared/themes/light.json'
+
+const ICON_CONFIG: Record<string, Icon> = {
+  dark: new Icon({
+    name: 'moon',
+    color: Object.values(light).at(0)
+  }),
+  light: new Icon({
+    name: 'sun',
+    color: Object.values(dark).at(0)
+  })
+}
 
 @Component({
   selector: 'menphys-theme-changer',
@@ -6,5 +21,21 @@ import { Component } from '@angular/core';
   styleUrls: [ './theme-changer.component.scss' ]
 })
 export class ThemeChangerComponent {
+
+  public theme = MenphysModule.config.theme;
+
+  public get icon(): Icon {
+    return ICON_CONFIG[ this.theme ];
+  }
+
+  @HostListener('click')
+  public handleChange(): void {
+    if (this.theme === 'dark') {
+      this.theme = 'light';
+    } else {
+      this.theme = 'dark';
+    }
+    MenphysModule.changeTheme(this.theme);
+  }
 
 }
